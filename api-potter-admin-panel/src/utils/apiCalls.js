9208f199795a989b23ac97027a61580415ebe6c8
saulm96 <saulmorahernandez96@gmail.com>
2025-01-08 12:31:08 +0100

    Delete function done

--- api-potter-admin-panel/src/utils/apiCalls.js
+++ api-potter-admin-panel/src/utils/apiCalls.js
@@ -23,4 +23,37 @@ async function fetchApiList(variable) {
     }
 }
 
-export default fetchApiList;
+//Function to fetch the api with the DELETE method
+async function fetchApiDelete(variable, id) {
+    const baseUrl = 'http://localhost:3001/api/';
+    const url = `${baseUrl}${variable}/${id}`;
+    console.log(url)
+
+    const token = localStorage.getItem('token');
+
+    try {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        })
+        console.log(response)
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+        
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+}
+export const apiCalls = {
+    fetchApiList,
+    fetchApiDelete
+};
+//export all functions in this file
+export default apiCalls;
